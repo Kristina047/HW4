deca066ce5b9c4695f31e0b54e13ea939b12a0f3
Kristina047 <ch.drobot@gmail.com>
2024-11-21 21:55:46 +0300

    условные операторы 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int clientOs = 1;
+        if (clientOs == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
     }
 }
+
+
+
+
+
+
+
