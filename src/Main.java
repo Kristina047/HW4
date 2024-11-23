8dfdb62cb8d994727ac499a7011c1b095f9307db
Kristina047 <ch.drobot@gmail.com>
2024-11-23 15:40:11 +0300

    условные операторы 2

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задача 1 и 2");
-        int clientOs = 1;
-        int clientDeviceYear = 2015;
+
+        int clientOs = 0;
+        int clientDeviceYear = 2017;
         if (clientOs == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOs == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if  (clientOs == 0 && clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Ios по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+
         }
     }
-}
+
 
 
 
